410759311eab57f8cbf5810ff8f3ea7eb310c017
PRITAM CHAKRABORTY <pritam.chk98@gmail.com>
2022-04-09 13:26:23 +0530

    quiz app ready

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,12 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link :to="{name : 'Quiz'}">Quiz</router-link> 
+  <div  class="flex flex-row my-2 text-center justify-center  ">
+    <router-link to="/" class="focus:bg-green-300 hover:bg-green-400 hover:text-white px-10 py-2 rounded-md mr-5 transition-all ease-in-out duration-300 ">Home</router-link> 
+    <router-link :to="{name : 'Quiz'} " class="hover:bg-green-400 hover:text-white  px-10 py-2 rounded-md transition-all ease-in-out duration-300">Quiz</router-link> 
   </div>
 
   <router-view/>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
--- src/views/Week.vue
+++ src/views/Week.vue
@@ -1,8 +1,8 @@
-<template>
+<template >
   <form @submit.prevent="submitFun()" class="accent-pink-500 space-y-4 flex flex-col">
-    <div class="justify-evenly text-left px-10 py-3 mx-10 shadow-md shadow-green-200/50 rounded-md" v-for="(quiz, im) in allQuiz" :key="quiz.qno">
+    <div class=" bg-slate-50 text-black justify-evenly text-left px-10 py-3 mx-10 shadow-md shadow-green-200/50 rounded-md" v-for="(quiz, im) in allQuiz" :key="quiz.qno">
       <h2 class="text-lg text-blue-700 font-bold">{{ quiz.qno }} . {{ quiz.statement }}</h2>
-      <div v-for="(option, index) in quiz.options" :key="index">
+      <div class="px-2 mx-1 py-3 shadow-md rounded-md my-4" v-for="(option, index) in quiz.options" :key="index">
         <input 
           v-if="!isSubmited"
           type="radio"
@@ -11,10 +11,10 @@
           :id="option.value + quiz.qno"
           v-model="formValues[im]"
         />
-        <label :for="option.value + quiz.qno">{{ option.value }}</label>
+        <label class="px-5"  :for="option.value + quiz.qno">{{ option.value }}</label>
       </div>
-      <div v-show="isSubmited">
-        <div class="bg-yellow-200/50 rounded-sm shadow-sm pl-5 py-3 m-1">
+      <div v-show="isSubmited" class="font-semibold">
+        <div class="bg-yellow-300/50  rounded-sm shadow-sm pl-5 py-3 m-1">
           <h2
             class="text-green-400"
             v-if="formValues[im].split(' ')[0] === 'true' ? true : false"
@@ -28,10 +28,19 @@
         </div>
       </div>
     </div>
-    <div>
-      <button class="transition-all duration-200 ease-in-out rounded-lg px-10 py-2 my-5 bg-lime-500 text-white hover:bg-lime-300 hover:text-slate-600" v-if="!isSubmited">Submit</button>
+    <div class="flex justify-center">
+      <button class="btn" v-if="!isSubmited">Submit</button>
     </div>
   </form>
+  <div class="flex justify-center">
+    <button 
+    class="btn"
+      v-show="isSubmited"
+      @click="redirect"
+    >
+    Go To Quiz
+    </button>
+  </div>
 </template>
 
 <script>
@@ -69,6 +78,11 @@ export default {
     submitFun() {
       this.isSubmited = true;
     },
+    redirect(){
+      this.$router.push({
+        name : 'Quiz'
+      })
+    }
   },
   computed: {
     shuffleArray(inputArray) {
