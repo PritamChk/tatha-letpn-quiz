7e9e73ead1dc2ec859968dfc80be808bbcc471a3
PRITAM CHAKRABORTY <pritam.chk98@gmail.com>
2022-04-10 13:49:48 +0530

    formatted

--- src/views/Quiz.vue
+++ src/views/Quiz.vue
@@ -1,27 +1,26 @@
 <template class="bg-black">
-<div class="flex flex-wrap overflow-hidden">
-    <template  v-for="week in weeks" :key="week">
-        <router-link :to="{name : 'Week' , params: {id: week}}">
-            <h2 
-            class="card text-lg font-medium text-center text-white "
-            >week-{{week}}</h2>
-        </router-link>
+  <div class="flex flex-wrap overflow-hidden">
+    <template v-for="week in weeks" :key="week">
+      <router-link :to="{ name: 'Week', params: { id: week } }">
+        <h2 class="card text-lg font-medium text-center text-white">
+          week-{{ week }}
+        </h2>
+      </router-link>
     </template>
-</div>
+  </div>
 </template>
 
 <script>
-import Week from './Week.vue'
+import Week from "./Week.vue";
 export default {
-    name: 'Quiz',
-    components:{
-        Week,
-    },
-    data(){
-        return{
-            weeks : 12
-        }
-    }
-
-}
+  name: "Quiz",
+  components: {
+    Week,
+  },
+  data() {
+    return {
+      weeks: 12,
+    };
+  },
+};
 </script>
